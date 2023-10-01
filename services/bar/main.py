7b149f5b917a050d13af521740278b476261b8ef
KittitatNM <61957547+KittitatNM@users.noreply.github.com>
2023-10-01 22:52:00 +0700

    first init grafana tempo

---
+++ services/bar/main.py
@@ -0,0 +1,138 @@
+# from time import sleep
+# from flask import Flask, request
+# from flask.logging import default_handler
+# from opentelemetry import trace
+# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+# import logging
+# import os
+# from random import random
+# from time import strftime
+
+# AGENT_HOSTNAME = os.getenv("AGENT_HOSTNAME", "localhost")
+# AGENT_PORT = int(os.getenv("AGENT_PORT", "4317"))
+
+
+# class SpanFormatter(logging.Formatter):
+#     def format(self, record):
+#         trace_id = trace.get_current_span().get_span_context().trace_id
+#         if trace_id == 0:
+#             record.trace_id = None
+#         else:
+#             record.trace_id = "{trace:032x}".format(trace=trace_id)
+#         return super().format(record)
+
+
+# resource = Resource(attributes={
+#     "service.name": "service-bar"
+# })
+
+# trace.set_tracer_provider(
+#     TracerProvider(resource=resource)
+# )
+# otlp_exporter = OTLPSpanExporter(endpoint=f"{AGENT_HOSTNAME}:{AGENT_PORT}", insecure=True)
+# trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+
+# app = Flask(__name__)
+
+# FlaskInstrumentor().instrument_app(app)
+
+# log = logging.getLogger("werkzeug")
+# log.setLevel(logging.ERROR)
+# app.logger.setLevel(logging.INFO)
+# default_handler.setFormatter(
+#     SpanFormatter(
+#         'time="%(asctime)s" service=%(name)s level=%(levelname)s %(message)s trace_id=%(trace_id)s'
+#     )
+# )
+
+
+# @app.route("/bar")
+# def bar():
+#     random_value = random()  # random number in range [0.0,1.0)
+#     if random_value < 0.05:
+#         return "Edge case!", 500
+#     return "bar"
+
+
+# @app.after_request
+# def after_request(response):
+#     app.logger.info(
+#         'addr="%s" method=%s scheme=%s path="%s" status=%s',
+#         request.remote_addr,
+#         request.method,
+#         request.scheme,
+#         request.full_path,
+#         response.status_code,
+#     )
+#     return response
+
+
+from fastapi import FastAPI,HTTPException
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+import requests
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+import logging
+import os
+from random import random
+from time import strftime
+
+AGENT_HOSTNAME = os.getenv("AGENT_HOSTNAME", "localhost")
+AGENT_PORT = int(os.getenv("AGENT_PORT", "4317"))
+BAR_ENDPOINT = os.getenv("BAR_ENDPOINT", "http://localhost:5001/bar")
+
+
+class SpanFormatter(logging.Formatter):
+    def format(self, record):
+        trace_id = trace.get_current_span().get_span_context().trace_id
+        if trace_id == 0:
+            record.trace_id = None
+        else:
+            record.trace_id = "{trace:032x}".format(trace=trace_id)
+        return super().format(record)
+
+resource = Resource(attributes={
+    "service.name": "service-bar"
+})
+
+trace.set_tracer_provider(
+    TracerProvider(resource=resource)
+)
+otlp_exporter = OTLPSpanExporter(endpoint=f"{AGENT_HOSTNAME}:{AGENT_PORT}", insecure=True)
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+
+
+app = FastAPI()
+
+FastAPIInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+log = logging.getLogger("werkzeug")
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+ch.setFormatter(SpanFormatter(
+        'time="%(asctime)s" service=%(name)s level=%(levelname)s %(message)s trace_id=%(trace_id)s'
+    ))
+log.addHandler(ch)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/bar")
+def bar():
+    random_value = random()  # random number in range [0.0,1.0)
+    if random_value < 0.05:
+        # return "Edge case!", 500
+        raise HTTPException(status_code=500, detail="test exception")
+    return {"Hello": "bar"}
